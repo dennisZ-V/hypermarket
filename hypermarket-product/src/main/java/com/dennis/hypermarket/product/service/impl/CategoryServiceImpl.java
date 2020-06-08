bfe4922ab6d28a5a8980df6b4545561cde321cff
Dennis.Zhang <dennis.zhang@mspbots.ai>
2020-06-08 18:29:28 +0800

    添加商品树查询

--- hypermarket-product/src/main/java/com/dennis/hypermarket/product/service/impl/CategoryServiceImpl.java
+++ hypermarket-product/src/main/java/com/dennis/hypermarket/product/service/impl/CategoryServiceImpl.java
@@ -1,7 +1,12 @@
 package com.dennis.hypermarket.product.service.impl;
 
 import org.springframework.stereotype.Service;
+
+import java.util.Comparator;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -24,6 +29,32 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
         );
 
         return new PageUtils(page);
+    }
+
+    @Override
+    public List<CategoryEntity> listWithTree() {
+        List<CategoryEntity> entities = baseMapper.selectList(null);
+
+        List<CategoryEntity> level1Menus = entities.stream().filter(categoryEntity ->
+                categoryEntity.getParentCid() == 0
+        ).map(menu -> {
+            menu.setChildren(getChildrens(menu, entities));
+            return menu;
+        }).sorted(Comparator.comparingInt(CategoryEntity::getSort)).collect(Collectors.toList());
+
+        return level1Menus;
+    }
+
+    private List<CategoryEntity> getChildrens(CategoryEntity root, List<CategoryEntity> all) {
+
+        List<CategoryEntity> children = all.stream().filter(categoryEntity ->
+                categoryEntity.getParentCid().equals(root.getCatId())
+        ).map(category -> {
+            category.setChildren(getChildrens(category, all));
+            return category;
+        }).sorted(Comparator.comparingInt(menu -> (menu.getSort() == null ? 0 : menu.getSort()))).collect(Collectors.toList());
+
+        return children;
     }
 
 }
